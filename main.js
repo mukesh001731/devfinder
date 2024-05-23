3ae50733ab1af64db3ab47a1ea90aa81d76d9c51
Mukesh Mali <67068171+mukesh001731@users.noreply.github.com>
2024-05-23 23:53:22 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const modeToggle = document.getElementById('modeToggle');
+    const icon = document.getElementById('icon');
+
+    modeToggle.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        if (document.body.classList.contains('dark-mode')) {
+            icon.classList.remove('fa-moon');
+            icon.classList.add('fa-sun');
+        } else {
+            icon.classList.remove('fa-sun');
+            icon.classList.add('fa-moon');
+        }
+    });
+});
 
